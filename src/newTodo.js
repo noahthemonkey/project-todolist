6204bafd848504a090e17b270a82e01b7587a719
Noah Msekni <msekninoah@gmail.com>
2022-10-21 17:42:55 +0200

    Add input elements for creating a new todo
    Input elements can be filled out by user to then create a new todo
    When the button is pressed all the inputs the user filled in will be
    in the new todo.

--- src/newTodo.js
+++ src/newTodo.js
@@ -1,15 +1,53 @@
 import {todoFactory} from './todoFactory.js'
 
 
-export const newTodo = () => {
+export const newTodo = () => { 
+    const inputContainer = document.createElement('div')
+    inputContainer.setAttribute('id', 'inputContainer')
+    content.appendChild(inputContainer)
+    const todoTitle = document.createElement('input')
+    todoTitle.setAttribute('id', 'todoTitle')
+    todoTitle.setAttribute('type', 'text')
+    todoTitle.setAttribute('placeholder', 'title')
+
+    inputContainer.appendChild(todoTitle)
+    const todoDescription = document.createElement('input')
+    todoDescription.setAttribute('id', 'todoDescription')
+    todoDescription.setAttribute('type', 'text')
+    todoDescription.setAttribute('placeholder', 'Description')
+    inputContainer.appendChild(todoDescription)
+    const todoNotes = document.createElement('input')
+    todoNotes.setAttribute('id', 'todoNotes')
+    todoNotes.setAttribute('type', 'text')
+    todoNotes.setAttribute('placeholder', 'Notes')
+    inputContainer.appendChild(todoNotes)
+    const todoPriority = document.createElement('input')
+    todoPriority.setAttribute('id', 'todoPriority')
+    todoPriority.setAttribute('type', 'text')
+    todoPriority.setAttribute('placeholder', 'Priority')
+    inputContainer.appendChild(todoPriority)
+    const todoDue = document.createElement('input')
+    todoDue.setAttribute('id', 'todoDue')
+    todoDue.setAttribute('type', 'text')
+    todoDue.setAttribute('placeholder', 'Due Date')
+    inputContainer.appendChild(todoDue)
     const newBtn = document.createElement('button')
     newBtn.setAttribute('id', 'newTodo')
     newBtn.addEventListener('click', createTodo)
 
-    content.appendChild(newBtn)
+    inputContainer.appendChild(newBtn)
 
     function createTodo() {
-        console.log('nice!')
+        
+        const creatingTodo = todoFactory(todoTitle.value, todoDescription.value, todoNotes.value, todoDue.value, todoPriority.value)
+
+        
+        for(let i = 0;i < 5; i++){
+            inputContainer.children.item(i).value = ''
+
+        }
+        
+
     }
 }
 
--- src/todoFactory.js
+++ src/todoFactory.js
@@ -2,12 +2,13 @@ import {factory} from './domfactory.js'
 
 export const todoFactory = (title, description, notes, dueDate, priority) => {
     // const todoStart = factory('div', {class:'todoStart'}, 'Todolist 1')
-const todoTitle = factory('div', {id: `todoTitle_${title}`}, `${title}`)
-const todoDescription = factory('p', {class: 'description'}, `${description}`)
-const todoNotes = factory('p', {class: `notes`}, `${notes}`)
-const tododueDate = factory('p', {class: `dueDate`}, `${dueDate}`)
-const todoPriority = factory('p', {class: `priority`}, `${priority}`)
+    const todoDiv = factory('div', {id: `${title}`})
+    const todoTitle = factory('h1', {id: `todoTitle_${title}`, contentEditable: 'true'}, `${title}`)
+    const todoDescription = factory('p', {class: 'description', contentEditable: 'true'}, `${description}`)
+    const todoNotes = factory('p', {class: `notes`, contentEditable: 'true'}, `${notes}`)
+    const tododueDate = factory('p', {class: `dueDate`, contentEditable: 'true'}, `${dueDate}`)
+    const todoPriority = factory('p', {class: `priority`, contentEditable: 'true'}, `${priority}`)
 
-todoTitle.append(todoDescription, todoNotes, tododueDate, todoPriority)
-todoContainer.appendChild(todoTitle)
+todoDiv.append(todoTitle, todoDescription, todoNotes, tododueDate, todoPriority)
+todoContainer.appendChild(todoDiv)
 }
